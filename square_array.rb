e8d105b9af103c45743d58fab2ab25411d5194ed
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-04 21:30:30 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,12 @@
+#def square_array(array)
+  #new_array = Array.new
+  #array.each do |num|
+  #  new_array << num**2
+  #end
+  #return new_array
+#end
+
 def square_array(array)
-  new_array = Array.new
-  array.each do |num|
-    new_array << num**2
+  array.collect do {|x| x**2}
   end
-  return new_array
 end
